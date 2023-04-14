196d6f2d462f59bad27df009b069c6ac04b9b05e
igorkishko <kishko.igor@gmail.com>
2023-04-14 18:21:19 +0300

    add new links to prismic client

--- src/components/Menu/prismic.js
+++ src/components/Menu/prismic.js
@@ -1,37 +1,45 @@
-import * as prismic from '@prismicio/client'
+import * as prismic from '@prismicio/client';
 
-export const repositoryName = 'airdao'
+export const repositoryName = 'airdao';
 
 export const client = prismic.createClient(repositoryName, {
-	accessToken: process.env.REACT_APP_PRISMIC_ACCESS_TOKEN,
-	routes: [
-		{
-			type: 'homepage',
-			path: '/'
-		},
-		{
-			type: 'bridge',
-			path: '/bridge'
-		},
-		{
-			type: 'firepot',
-			path: '/firepot'
-		},
-		{
-			type: 'staking',
-			path: '/staking'
-		},
-		{
-			type: 'explorer',
-			path: '/explorer',
-		},
-		{
-			type: 'team',
-			path: '/team'
-		},
-		{
-			type: 'binance',
-			path: '/binance'
-		}
-	],
-})
+  accessToken: process.env.REACT_APP_PRISMIC_ACCESS_TOKEN,
+  routes: [
+    {
+      type: 'homepage',
+      path: '/',
+    },
+    {
+      type: 'bridge',
+      path: '/bridge',
+    },
+    {
+      type: 'firepot',
+      path: '/firepot',
+    },
+    {
+      type: 'staking',
+      path: '/staking',
+    },
+    {
+      type: 'explorer',
+      path: '/explorer',
+    },
+    {
+      type: 'team',
+      path: '/team',
+    },
+    {
+      type: 'binance',
+      path: '/binance',
+    },
+    {
+      type: 'airdrop',
+      path: '/airdrop',
+    },
+    {
+      type: 'bondmarketplace',
+      path: '/bond-marketplace',
+    },
+  ],
+});
